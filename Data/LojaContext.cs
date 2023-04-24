6ad0030ba25a51523ddbf91f2d953bbaf184dbd7
Richard <richardfelipewt@hotmail.com>
2023-04-24 10:32:18 -0300

    Adicionando a Migration e adicionando as propriedades no LojaContext

--- Data/LojaContext.cs
+++ Data/LojaContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Loja.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Loja.Data;
 public class LojaContext : DbContext
 {
-
+    public DbSet<Produto> Produto { get; set; }
+    public DbSet<Compra> Compra { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
         options.UseSqlServer("Server=127.0.0.1;Database=Loja;User Id=sa;Password=Dev$123;Encrypt=False;");
---
+++ Migrations/20230424132926_TabelasIniciais.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loja.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelasIniciais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Categoria = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrecoUnitario = table.Column<double>(type: "float", nullable: false),
+                    Unidade = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Compra",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    ProdutoId = table.Column<int>(type: "int", nullable: false),
+                    Preco = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compra", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Compra_Produto_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compra_ProdutoId",
+                table: "Compra",
+                column: "ProdutoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compra");
+
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
