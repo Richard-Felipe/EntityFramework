dabc22c38120159101f30a7e5e47ae6799e91806
Richard <richardfelipewt@hotmail.com>
2023-04-24 09:04:19 -0300

    Criando o contexto do Entity, herdando a classe DbContext para utilização da biblioteca e configurando o SqlServer no método OnConfiguring

---
+++ Data/LojaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Loja.Data;
+public class LojaContext : DbContext
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("Server=127.0.0.1;Database=Loja;User Id=sa;Password=Dev$123;Encrypt=False;");
+    }
+}
