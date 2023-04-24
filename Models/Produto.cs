8909bb3e46f3c16979dc8429e87bd771987eda16
Richard <richardfelipewt@hotmail.com>
2023-04-24 09:13:13 -0300

    Adicionando a Classe Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,1 +1,9 @@
-﻿
+﻿namespace Loja.Models;
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; internal set; }
+    public string? Categoria { get; set; }
+    public double PrecoUnitario { get; set; }
+    public string? Unidade { get; set; }
+}
